d10c3a3c36429388f4479493034a2b0a25b15329
doudou459 <459349489@qq.com>
2020-02-03 20:01:40 +0800

    2020-2-3

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import MintUI from 'mint-ui';
 import 'mint-ui/lib/style.css';
 Vue.use(ElementUI);
 Vue.use(MintUI);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
